c14f09c2f77c5267cabf0e2b872c9ed62f61edca
jeslynzhou <jeslynzhou0618@gmail.com>
2024-05-21 02:22:35 +0800

    fixing Authentication error

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
-import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from '@firebase/auth';
-
+import { initializeApp } from '@firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgDWZ2GeJMqMqY_fXKcGHFc7wUJPGJchw",
-  authDomain: "pawllywood-new.firebaseapp.com",
-  projectId: "pawllywood-new",
-  storageBucket: "pawllywood-new.appspot.com",
-  messagingSenderId: "708339902035",
-  appId: "1:708339902035:web:639666e697780e35de88a6"
+  apiKey: "AIzaSyCGHpSfjU4CJ02RtoGp_01j5cF6P629epg",
+  authDomain: "pawllywood-abc3e.firebaseapp.com",
+  projectId: "pawllywood-abc3e",
+  storageBucket: "pawllywood-abc3e.appspot.com",
+  messagingSenderId: "62836081451",
+  appId: "1:62836081451:web:7ccb964474f61b6c5c3b6e",
+  measurementId: "G-5GGHJXB9BP"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const AuthScreen = ({ email, setEmail, password, setPassword, isLogin, setIsLogin, handleAuthentication }) => {
